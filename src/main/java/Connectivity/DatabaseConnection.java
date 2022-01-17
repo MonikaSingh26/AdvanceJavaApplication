68cc4a7a7d4d1b9affe2c8e62834196bfa993cbb
Monika Singh <monikasingh0126@gmail.com>
2022-01-17 22:20:59 +0530

    Modified code in Database Connectivity.

--- src/main/java/Connectivity/DatabaseConnection.java
+++ src/main/java/Connectivity/DatabaseConnection.java
@@ -1,32 +1,23 @@
 package Connectivity;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseConnection {
-        public static Connection createConnection()
-        {
+        public static Connection getConnection() {
             Connection connection = null;
             String url = "jdbc:mysql://localhost:3306/application"; // MySQL URL and followed by the Database name
             String username = "root"; // MySQL username
             String password = "123456"; // MySQL password
-            try
-            {
-                try
-                {
+                try {
                     Class.forName("com.mysql.cj.jdbc.Driver"); // Loading MySQL driver
-                }
-                catch (ClassNotFoundException exception)
-                {
+                    connection = DriverManager.getConnection(url, username, password); // Attempting to connect to MySQL Database
+                    System.out.println("Database Connection Established");
+                } catch (SQLException | ClassNotFoundException exception) {
                     exception.printStackTrace();
                 }
-                connection = DriverManager.getConnection(url, username, password); // Attempting to connect to MySQL Database
-                System.out.println("Database Connection Established");
-            }
-            catch (Exception exception)
-            {
-                exception.printStackTrace();
-            }
             return connection;
         }
 }
